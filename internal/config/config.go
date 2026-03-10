256bb95156b95058ef3befd00cb9fdefe9a15c73
devvdark0 <vladmasliev777@gmail.com>
2026-03-10 14:44:38 +0300

    add db configuration

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,29 +1,47 @@
 package config
 
 import (
+	"os"
 	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	App AppConfig
+	App AppConfig `yaml:"app"`
+	Db  DbConfig  `yaml:"db"`
 }
 
 type AppConfig struct {
-	Env         string
-	Host        string
-	Port        string
-	Timeout     time.Duration
-	IdleTimeout time.Duration
+	Env         string        `yaml:"env"`
+	Host        string        `yaml:"host"`
+	Port        string        `yaml:"port"`
+	Timeout     time.Duration `yaml:"timeout"`
+	IdleTimeout time.Duration `yaml:"idle_timeout"`
+}
+
+type DbConfig struct {
+	Name     string `yaml:"name"`
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	Username string `yaml:"user"`
+	Password string `env:"DB_PASSWORD"`
+	SSLMode  string `yaml:"sslmode"`
 }
 
 func MustLoad(cfgPath string) (*Config, error) {
+	if err := godotenv.Load(); err != nil {
+		return nil, err
+	}
+
 	var cfg Config
 
 	if err := cleanenv.ReadConfig(cfgPath, &cfg); err != nil {
 		return nil, err
 	}
+
+	cfg.Db.Password = os.Getenv("DB_PASSWORD")
 
 	return &cfg, nil
 }
