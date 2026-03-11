c0392ed954c14f47d015e261941c73b71c6b96fa
devvdark0 <vladmasliev777@gmail.com>
2026-03-11 13:34:24 +0300

    add password hashing + verifying password

---
+++ internal/auth/passwords.go
@@ -0,0 +1,16 @@
+package auth
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	passHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(passHash), nil
+}
+
+func VerifyPasssword(origPassword, hashPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashPassword), []byte(origPassword))
+}
