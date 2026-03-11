76767c85792971ec23ccbb52bd42bde0eab41fea
devvdark0 <vladmasliev777@gmail.com>
2026-03-11 10:46:19 +0300

    configure logger

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -4,9 +4,16 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/devvdark0/auth-service/internal/config"
+)
+
+const (
+	envDev  = "dev"
+	envProd = "prod"
 )
 
 func main() {
@@ -17,9 +24,12 @@ func main() {
 		log.Fatal(err)
 	}
 
+	log := configureLogger(cfg.App.Env)
+
+	log.Debug("initializing db....")
 	db, err := config.InitDb(&cfg.Db)
 	if err != nil {
-		log.Fatal(err)
+		log.Error("failed to init databaase", "err", err)
 	}
 	_ = db
 
@@ -31,8 +41,25 @@ func main() {
 		IdleTimeout:  cfg.App.IdleTimeout,
 	}
 
-	log.Print("starting server....", "port=", cfg.App.Port)
+	log.Info("starting server...", "port", cfg.App.Port)
 	if err := srv.ListenAndServe(); err != nil {
 		panic(err)
 	}
+}
+
+func configureLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envDev:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		}))
+	}
+
+	return log
 }
