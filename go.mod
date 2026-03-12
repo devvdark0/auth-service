c20ed32db441fbaddf3c50feb2a8463de234dab3
devvdark0 <vladmasliev777@gmail.com>
2026-03-12 09:41:40 +0300

    add token creation and validation logic

--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/joho/godotenv v1.5.1
 )
 
+require github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+
 require (
 	github.com/BurntSushi/toml v1.6.0 // indirect
 	github.com/lib/pq v1.11.2 // indirect
---
+++ internal/auth/jwt.go
@@ -0,0 +1,60 @@
+package auth
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/devvdark0/auth-service/internal/config"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JWTManager struct {
+	secret   []byte
+	tokenTTL time.Duration
+}
+
+func NewJWTManager(cfg *config.AuthConfig) *JWTManager {
+	return &JWTManager{
+		secret:   cfg.Secret,
+		tokenTTL: cfg.TokenTTL,
+	}
+}
+
+func (j *JWTManager) GenerateToken(id int64, email string) (string, error) {
+	exp := time.Now().Add(j.tokenTTL)
+
+	claims := jwt.MapClaims{
+		"sub":   id,
+		"email": email,
+		"iat":   time.Now().Unix(),
+		"exp":   exp.Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenStr, err := token.SignedString(j.secret)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenStr, nil
+}
+
+func (j *JWTManager) ValidateToken(tokenStr string) (*jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (any, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return "", fmt.Errorf("invalid token")
+		}
+
+		return j.secret, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*jwt.MapClaims); ok {
+		return claims, nil
+	}
+
+	return nil, fmt.Errorf("invalid token")
+}
